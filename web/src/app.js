5aa59b94dc94f5b4f5f8b768849b3833df7b556a
Jerome Terry <jerome.l.terry@gmail.com>
2015-05-03 17:28:00 -0230

    Getting containers working
    Passing URL to the Brewski API as an environment variable to the web
    docker container. Since the URL must be accessible from a web browser,
    we can’t just use the internal name or ip address of the api container.
    
    Renamed index.js to app.js, since index.js seems to me to indicate that
    it’s being served from a web server (which it isn’t).

--- web/src/app.js
+++ web/src/app.js
@@ -1,5 +1,6 @@
 var server = require('./server.js');
 var DEFAULT_PORT = 8081;
 var port = process.env.PORT || DEFAULT_PORT;
-var apiUrl = "http://localhost:8080";
+var node_env = process.env.NODE_ENV;
+var apiUrl = process.env.API_URL || "http://localhost:8080";
 server.start(port, apiUrl);
