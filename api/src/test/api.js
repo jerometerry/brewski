216d3aef05e9425e5bd99f16d74e28b4b9248976
Jerome Terry <jerome.l.terry@gmail.com>
2015-05-04 08:37:25 -0230

    Assigning URL outside of the test

--- api/src/test/api.js
+++ api/src/test/api.js
@@ -4,11 +4,12 @@ var assert = require('chai').assert,
     request = require('supertest');
 
 var url = "http://localhost:8080";
+request = request(url);
 
 describe('API', function() {  
    describe('GET beers', function () {
       it('should return beer list', function (done) {
-         request(url)
+         request
             .get('/api/beers/')
             .end(function(err, res){
                assert(res)
